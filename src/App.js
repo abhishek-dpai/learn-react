c50ec3fb656333a6e39d3c42d6691ea319b3985b
raiab <abhishekr@dailyprophet.ai>
2020-09-29 14:14:16 +0530

    realtodo first commit

--- src/App.js
+++ src/App.js
@@ -5,19 +5,9 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import ToDoItems from "./components/ToDoItems";
 function App() {
-  const date = new Date();
-  const hours = date.getHours();
-  let timeOfDay;
-  if (hours > 12) timeOfDay = "morning";
-  else if (hours < 17) timeOfDay = "afternoon";
-  else timeOfDay = "night";
   return (
     <div>
-      <h1>{` ${date.getHours()}:${date.getMinutes()} `} !</h1>
-      <h1>Good {timeOfDay} !</h1>
-      <Header />
-      <MainContent />
-      <Footer />
+      <ToDoItems />
     </div>
   );
 }
