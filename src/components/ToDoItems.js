1252f088da401b06bad736d229fcc41e52059794
raiab <abhishekr@dailyprophet.ai>
2020-09-29 14:12:13 +0530

    todo branch last commit

---
+++ src/components/ToDoItems.js
@@ -0,0 +1,14 @@
+import React from "react";
+function ToDoItems() {
+  return (
+    <div>
+      <input type="checkbox" />
+      <p>Placeholder 1 text here</p>
+      <input type="checkbox" />
+      <p>Placeholder 2 text here</p>
+      <input type="checkbox" />
+      <p>Placeholder 3 text here</p>
+    </div>
+  );
+}
+export default ToDoItems;
